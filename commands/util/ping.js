8d747103242336e51704644c3c6c509ad6e8925c
Siddharth Arumugam <9015974-0xMrNight@users.noreply.gitlab.com>
2021-06-04 15:38:48 +0530

    Create ping command

---
+++ commands/util/ping.js
@@ -0,0 +1,21 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class PingCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ping',
+            group: 'util',
+            memberName: 'ping',
+            description: 'Checks the bot\'s ping time',
+            throttling: {
+                usages: 2,
+                duration: 10,
+            }
+        });
+    }
+
+    async run(message) {
+        const pingMsg = await message.say('Pinging...');
+        return pingMsg.edit(`:ping_pong: Pong! \`${Math.round(this.client.ws.ping)}ms\``);
+    }
+}
