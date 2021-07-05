946b82acc4209d43dfc32cb56a0990e23882e0ff
Siddharth Arumugam <0xmrnight@gmail.com>
2021-07-05 13:01:10 +0530

    Add purge command

---
+++ commands/mod/purge.js
@@ -0,0 +1,35 @@
+const { Command } = require("discord.js-commando");
+
+module.exports = class PurgeCommand extends Command {
+    constructor(props){
+        super(props, {
+            name: 'purge',
+            group: 'mod',
+            memberName: 'purge',
+            description: 'Clears a number of messages',
+            aliases: ['clear', 'clr'],
+            guildOnly: true,
+            args: [
+                {
+                    key: 'number',
+                    prompt: 'Number of messages to clear',
+                    type: 'integer',
+                    default: 2,
+                    max: 100
+                }
+            ],
+            clientPermissions: ["MANAGE_MESSAGES"],
+            userPermissions: ["MANAGE_MESSAGES"]
+        })
+    }
+
+    async run(message, { number }) {
+        message.channel.bulkDelete(number + 1)
+            .then(msgs => {
+                message.reply(`Purged \`${number}\` messages`);
+            })
+            .catch(e => {
+                message.reply("An error occured.")
+            })
+    }
+}
