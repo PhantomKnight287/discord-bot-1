e0cb4b356e4183e18b3b56d1766dcd166cf9b303
Siddharth Arumugam <0xmrnight@gmail.com>
2021-07-08 12:01:18 +0530

    Move the client to a class

---
+++ struct/PhiDiscordBot.js
@@ -0,0 +1,41 @@
+const { CommandoClient } = require("discord.js-commando");
+const { join } = require("path");
+require("dotenv").config();
+
+class PhiDiscordBot extends CommandoClient {
+  constructor(props) {
+    super(props);
+    this.config = require("../config.json");
+    if (process.env.TOKEN == "") {
+      return new TypeError("Please enter the bot's token in the .env file.");
+    }
+
+    // Options for Commando Client
+    this.commandPrefix = this.config.prefix;
+    this.owner = this.config.ownerID;
+    this.invite = this.config.serverInvite;
+
+    this.on("ready", async () => {
+      console.log(`I have logged in as ${this.user.tag} (id: ${this.user.id})`);
+
+      this.registry
+        .registerDefaultTypes()
+        .registerDefaultGroups()
+        .registerGroups([
+          ["util", "Basic utility commands"],
+          ["mod", "Moderation commands"],
+        ])
+        .registerDefaultCommands({
+          ping: false,
+          eval: false,
+        })
+        .registerCommandsIn(join(__dirname, "../commands"));
+    });
+  }
+
+  start() {
+    this.login(process.env.TOKEN);
+  }
+}
+
+module.exports = PhiDiscordBot;
